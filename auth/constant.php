c2dcc80fed88c70899ce4fcf08affac0fb2b3b4f
alendysenda <alendysenda@gmail.com>
2013-10-17 11:47:52 +0700

    add constant

--- auth/constant.php
+++ auth/constant.php
@@ -1,10 +1,8 @@
 <?php
-// define constant for databse configuration
+// constanta buat dipake di auth.php dll
 define("DB_SERVER","Server name");
 define("DB_USER","User name");
 define("DB_PASS","Database Password");
 define("DB_NAME","Database Name");
-
-
 
 ?>
